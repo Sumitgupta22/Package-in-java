03d4a094b89ae96d5466798d8ea235f81f21564a
Sumit Gupta <131431222+Sumitgupta22@users.noreply.github.com>
2024-03-28 06:06:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>Pakages in java</h1><hr>
+<ul>
+  <li> Create file With this line <b>package packagename;</b></li>
+  <li> After this go to shell and Write This Line javac -d . filename.java</li>
+  <li> if for import this file , create a new file with this line<b> import packagename.filename; </b>or <b>import packagename.*; </b></li>
+</ul>
+<hr>
+<h3>Thankyou so much </h3>
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import aiohs.hello;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    hello obj = new hello();
+    obj.text();
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
